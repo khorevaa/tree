fdf8102eebd1bdd4ce79f4ac1175d8f22b037934
lunjon <jonathan.lundholm@hotmail.com>
2018-11-09 21:40:55 +0100

    Extract node to own file

---
+++ node.go
@@ -0,0 +1,45 @@
+package tree
+
+import "fmt"
+
+// Node is the data structure which the tree is consists of
+type Node struct {
+	Value    interface{}
+	Parent   *Node
+	Children []*Node
+}
+
+// Add adds a new node under the receiving node with the specified value
+func (node *Node) Add(value interface{}) *Node {
+	// TODO: Check for nil value and return an error?
+
+	new := &Node{value, node, []*Node{}}
+	node.Children = append(node.Children, new)
+	return new
+}
+
+// Remove deletes this node from the tree and all its descdendants
+func (node *Node) Remove() {
+	// Check if it's the root
+	if node.Parent == nil {
+		return // Do nothing
+	}
+
+	// Remove this node from its parent
+	parent := node.Parent
+	for i, child := range parent.Children {
+		if child == node {
+			parent.Children = append(parent.Children[:i], parent.Children[i+1:]...)
+			break
+		}
+	}
+}
+
+// HasChildren returns true if node has any child nodes
+func (node *Node) HasChildren() bool {
+	return len(node.Children) != 0
+}
+
+func (node *Node) String() string {
+	return fmt.Sprint(node.Value)
+}
--- tree.go
+++ tree.go
@@ -10,13 +10,6 @@ type Tree struct {
 	Root *Node
 }
 
-// Node is the data structure which the tree is consists of
-type Node struct {
-	Value    interface{}
-	Parent   *Node
-	Children []*Node
-}
-
 // Create returns a new empty tree
 func Create() *Tree {
 	root := Node{Value: nil, Parent: nil, Children: []*Node{}}
@@ -68,39 +61,4 @@ func recString(node *Node, nbrIndents int, b *strings.Builder) {
 			recString(child, nbrIndents+1, b)
 		}
 	}
-}
-
-// Add adds a new node under the receiving node with the specified value
-func (node *Node) Add(value interface{}) *Node {
-	// TODO: Check for nil value and return an error?
-
-	new := &Node{value, node, []*Node{}}
-	node.Children = append(node.Children, new)
-	return new
-}
-
-// Remove deletes this node from the tree and all its descdendants
-func (node *Node) Remove() {
-	// Check if it's the root
-	if node.Parent == nil {
-		return // Do nothing
-	}
-
-	// Remove this node from its parent
-	parent := node.Parent
-	for i, child := range parent.Children {
-		if child == node {
-			parent.Children = append(parent.Children[:i], parent.Children[i+1:]...)
-			break
-		}
-	}
-}
-
-// HasChildren returns true if node has any child nodes
-func (node *Node) HasChildren() bool {
-	return len(node.Children) != 0
-}
-
-func (node *Node) String() string {
-	return fmt.Sprint(node.Value)
 }
